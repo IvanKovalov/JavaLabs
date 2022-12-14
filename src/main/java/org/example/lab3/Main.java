8815fdb80b151e9e7cf56f94ee1f8e9f7aef2daa
IvanKovalov <kovalevvana8@gmail.com>
2022-12-14 20:15:09 +0300

    + added main class to lab3

---
+++ src/main/java/org/example/lab3/Main.java
@@ -0,0 +1,16 @@
+package org.example.lab3;
+
+
+
+public class Main {
+
+    // C17 = 16
+    // C3 = 2
+    public static void main(String[] args) {
+        TextHandler textHandler = new TextHandler();
+        textHandler.execute("The Java Development Kit (JDK) is a distribution " +
+                "of Java Technology by Oracle Corporation. It implements the Java Language Specification (JLS)" +
+                " and the Java Virtual Machine Specification (JVMS) and provides the Standard Edition (SE) of thee\n" +
+                " Java Application Programming Interface (API).");
+    }
+}
